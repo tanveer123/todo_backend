c1a133ee009c66a3c1846b07032b44aa1e300994
tanveer123 <tanveer_411393@hotmail.com>
2024-02-08 23:53:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-/*mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     const PORT = process.env.PORT || 8000
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
 }).catch(err => {
     console.log(err);
-});*/
-const PORT = process.env.PORT || 8000
+});
+/*const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`App is Listening on PORT ${PORT}`);
-})
+})*/
 // route
 app.get("/", (req, res) => {res.status(201).json({ message: " yes , Connected to Backend!" });
 });
