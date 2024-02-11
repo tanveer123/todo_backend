4592b095722b3250d841c65600d4fe272b45c9e2
tanveer123 <tanveer_411393@hotmail.com>
2024-02-11 22:28:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,34 @@ const cors = require("cors");
 require("dotenv").config();
 
 // middleware
-const corsOptions = {
+/*const corsOptions = {
     //origin: "http://localhost:3000" // frontend URI (ReactJS)
     origin: "https://todo-frontend-pearl-one.vercel.app",
     methods: ["POST","GET"],
     credentials: true
-}
+}*/
 app.use(express.json());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+
+// Allow CORS middleware
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+// Apply CORS middleware to all routes
+app.use(allowCors);
+
 
 // connect MongoDB
 mongoose.connect(process.env.MONGODB_URI).then(() => {
